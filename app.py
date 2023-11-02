0f50afc2369c98cf85b4414fb4c12f4fa5ed123b
Shashi sah <shashi.sah2002@gmail.com>
2023-11-02 12:27:12 +0530

    made changes to preprocessing and app

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 import streamlit as st
+import preprocessor
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
+
+    st.dataframe(df)
+
+    #Fetch unique users
+    user_list =  df['user'].unique().tolist()
+    st.sidebar.selectbox("Show analysis wrt", user_list)
