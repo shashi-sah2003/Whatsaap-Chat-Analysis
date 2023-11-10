7f1913e24e138815e25e7782c210d4424822d356
Shashi sah <shashi.sah2002@gmail.com>
2023-11-10 12:07:58 +0530

    Added helper and modified app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import preprocessor
+import preprocessor, helper
 
 st.sidebar.title("Whatsapp Chat Analyzer")
 
@@ -13,4 +13,26 @@ if uploaded_file is not None:
 
     #Fetch unique users
     user_list =  df['user'].unique().tolist()
-    st.sidebar.selectbox("Show analysis wrt", user_list)
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0,"Overall")
+
+    selected_user = st.sidebar.selectbox("Show analysis wrt", user_list)
+
+    if st.sidebar.button("Show Analysis"):
+        
+        num_messages, words, num_media_messages = helper.fetch_stats(selected_user, df)
+
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header("Total messages")
+            st.title(num_messages)
+
+        with col2:
+            st.header("Total words")
+            st.title(words)
+        
+        with col3:
+            st.header("Media shared")
+            st.title(num_media_messages)
